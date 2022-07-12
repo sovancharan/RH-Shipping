ff10763055ec7e773a66b1c3285f2e2d3a62116a
Sovan Charan <mail2sovancharan@gmail.com>
2022-07-12 10:42:14 +0530

    first commit

---
+++ Js/custom.js
@@ -0,0 +1,53 @@
+
+    $(' #owl-carousel1').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+$('#owl-carousel2 ').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+$('#owl-carousel3 ').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
